658ceaa2c3d66fb2200f75adc18025034218dc65
LangatD <55713390+LangatD@users.noreply.github.com>
2025-07-24 18:54:08 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-  anchor.addEventListener('click', function(e) {
-    e.preventDefault();
-    document.querySelector(this.getAttribute('href')).scrollIntoView({
-      behavior: 'smooth'
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+        if (window.innerWidth <= 768) {
+            document.querySelector('.nav-menu').classList.remove('active');
+        }
     });
-  });
+});
+
+document.querySelector('.menu-toggle').addEventListener('click', function() {
+    document.querySelector('.nav-menu').classList.toggle('active');
 });
